2cd896c17d58b893d0dc04997094c7cf4a38f914
arikallis-j <chernosov.vd@phystech.edu>
2021-10-08 12:11:02 +0300

    1.2 Танец красивых шариков

--- 7/balls.py
+++ 7/balls.py
@@ -1,10 +1,12 @@
 import pygame
 from pygame.draw import *
 from random import randint
+from math import sin, cos, pi
 pygame.init()
 
-FPS = 1
-screen = pygame.display.set_mode((1200, 900))
+FPS = 60
+A, B = 1200, 900
+win = pygame.display.set_mode((A,B))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -16,15 +18,57 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 count = 0
+aboard = 5
+N = 50
 
+BALLS = []
 
-def new_ball():
-    global x, y, r
-    x = randint(100, 1100)
-    y = randint(100, 900)
+def create_ball():
+    speed = 5
     r = randint(10, 100)
+    alpha = randint(0, 360)
+    x = randint(r, A-r)
+    y = randint(r, B-r)
+    vx = round(speed*cos(alpha*pi/180))
+    vy = round(speed*sin(alpha*pi/180))
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    return {
+            'r': r,
+            'xy': [x,y],
+            'v': [vx,vy],
+            'color': color
+            }
+
+def balls_full():
+    if len(BALLS)==0:
+        while len(BALLS)<N:
+            BALLS.append(create_ball())
+
+def draw(obj):
+    circle(win, 
+           obj['color'], 
+           obj['xy'], 
+           obj['r'])
+
+def move(obj):
+    x,y = obj['xy']
+    r = obj['r']
+    xwall = x < (aboard + r) or x > (A - aboard - r)
+    ywall = y < (aboard + r) or y > (B - aboard - r)
+    if xwall:
+        obj['v'][0] = (-1)*obj['v'][0]
+    if ywall:
+        obj['v'][1] = (-1)*obj['v'][1]
+
+    obj['xy'][0] += obj['v'][0]
+    obj['xy'][1] += obj['v'][1]
+
+
+def dance():
+    balls_full()
+    for ball in BALLS:
+        draw(ball)
+        move(ball)
 
 def click():
     global count
@@ -44,10 +88,10 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-
             click()
-    new_ball()
+    dance()    
+
     pygame.display.update()
-    screen.fill(BLACK)
+    win.fill(BLACK)
 
 pygame.quit()
