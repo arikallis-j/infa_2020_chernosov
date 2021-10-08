956c3614c70d44dfe0771c96280880e6c6a258ef
arikallis-j <chernosov.vd@phystech.edu>
2021-10-08 06:52:09 +0300

    Наложение изображений по последнему тыку

--- 6/make-a-person.py
+++ 6/make-a-person.py
@@ -35,42 +35,57 @@ mouth = [0,0]
 nose = [0,0]
 eyeLeft = [0,0]
 eyeRight = [0,0]
-parts = [None,body,head, hand, hair, mouth, nose, eyeLeft, eyeRight]
-n = 1
+parts = [body,head, hand, hair, mouth, nose, eyeLeft, eyeRight]
+J = [7,6,5,4,3,2,1,0]
+n = 0
+
+
+#обновление порядка покрытия
+def update():
+    global n
+    global J
+    AlphaBet = J
+    AlphaBet.remove(n)
+    AlphaBet.insert(7,n)
+    J = AlphaBet
+
 
 #функция отрисовки
 def drawWindow():
     win.blit(bg, (0,0))
-    for i in range(8):
-        win.blit(parts_img[i],(parts[i+1][0],parts[i+1][1]))
+    for j in range(8):
+        for i in range(8):
+            if i==J[j]:
+                win.blit(parts_img[i],(parts[i][0],parts[i][1]))
 
-#функция проверки
+#функция проверки выхода
 def check_stand():
     global run
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+#функция проверки клавищ
 def check_move():
     global n
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_1]:
-        n = 1
+        n = 0
     elif keys[pygame.K_2]:
-        n = 2
+        n = 1
     elif keys[pygame.K_3]:
-        n = 3
+        n = 2
     elif keys[pygame.K_4]:
-        n = 4
+        n = 3
     elif keys[pygame.K_5]:
-        n = 5
+        n = 4
     elif keys[pygame.K_6]:
-        n = 6
+        n = 5
     elif keys[pygame.K_7]:
-        n = 7
+        n = 6
     elif keys[pygame.K_8]:
-        n = 8
+        n = 7
     elif keys[pygame.K_0]:
         exit()
 
@@ -98,6 +113,7 @@ while run:
     clock.tick(FPS)
     check_stand()
     check_move()
+    update()
     drawWindow()
     pygame.display.update()
 
