db0a15d492bb1034e24e6a4fcc5f8ed6b806f153
arikallis-j <chernosov.vd@phystech.edu>
2021-10-08 18:07:30 +0300

    3.0 Добавлены коробки и уровни

--- 7/balls.py
+++ 7/balls.py
@@ -4,7 +4,7 @@ from random import randint
 from math import sin, cos, pi
 pygame.init()
 
-FPS = 60
+FPS = 120
 A, B = 1200, 900
 win = pygame.display.set_mode((A,B))
 
@@ -18,13 +18,28 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 widthLine = 3
-font = pygame.font.SysFont('serif', 36)
+font = pygame.font.SysFont('serif', 50)
 
 count = 0
 aboard = 5
-N = 5
+paragraph = 50
+
+level = "hard"
+if level == "hard":
+    speed = 5
+elif level == "normal":
+    speed = 3
+elif level == "easy":
+    speed = 2
+else:
+    speed = 0
+
+Nballs = 5
+Nboxes = 5
 
 BALLS = []
+BOXES = []
+
 def aboards():
     pygame.draw.lines(win, 
                       YELLOW, 
@@ -34,13 +49,34 @@ def aboards():
                        (aboard - widthLine + B, - aboard + widthLine + B),
                        (aboard - widthLine, - aboard + widthLine + B)],
                        width = widthLine)
+    pygame.draw.lines(win, 
+                      YELLOW, 
+                      True,
+                      [(aboard - widthLine + B, aboard - widthLine),
+                       (-aboard + widthLine + A, aboard - widthLine),
+                       (-aboard + widthLine + A, - aboard + widthLine + B),
+                       (aboard - widthLine + B, - aboard + widthLine + B)],
+                       width = widthLine)
+    pygame.draw.lines(win, 
+                      YELLOW, 
+                      True,
+                      [(aboard - widthLine + B, 100),
+                       (aboard - widthLine + A, 100)],
+                       width = widthLine)
+
 def table():
-    score_text = "Ваш счёт: " + str(count)
-    score = font.render(score_text, True, YELLOW)
-    win.blit(score, (B + aboard, 0))
+    level_text = "Level: " + level
+    score_text = "Score: " + str(count)
+
+    Score = font.render(score_text, True, YELLOW)
+    Level = font.render(level_text, True, YELLOW)
+    win.blit(Score, (B + 2*aboard, 0))
+    win.blit(Level, (B + 2*aboard, paragraph))
+
+
 
 def create_ball():
-    speed = 5
+    global speed
     r = randint(10, 100)
     alpha = randint(0, 360)
     x = randint(r, B-r)
@@ -49,27 +85,60 @@ def create_ball():
     vy = round(speed*sin(alpha*pi/180))
     color = COLORS[randint(0, 5)]
     return {
+            'type': "ball",
             'r': r,
             'xy': [x,y],
             'v': [vx,vy],
-            'color': color
+            'color': color,
+            'boards': [aboard + r, B - aboard - r, aboard + r, B - aboard - r],
+            'count': 1
+            }
+
+def create_box():
+    global speed
+    r = randint(10, 100)
+    alpha = randint(0, 360)
+    x = randint(r, B-r)
+    y = randint(r, B-r)
+    vx = round(speed*cos(alpha*pi/180) + speed**2/2)
+    vy = round(speed*sin(alpha*pi/180) + speed**2/2)
+    color = COLORS[randint(0, 5)]
+    return {
+            'type': "box",
+            'r': r,
+            'xy': [x,y],
+            'xyrr': [x, y ,r ,r],
+            'v': [vx,vy],
+            'boards': [aboard,B -r,aboard, B - aboard - r],
+            'color': color,
+            'count': 5
             }
 
 def balls_full():
-    while len(BALLS)<N:
+    while len(BALLS)<Nballs:
         BALLS.append(create_ball())
 
-def draw(obj):
+def boxes_full():
+    while len(BOXES)<Nboxes:
+        BOXES.append(create_box())
+
+def drawBase(obj):
     circle(win, 
            obj['color'], 
            obj['xy'], 
            obj['r'])
 
+def drawOther(obj):
+    rect(win, 
+         obj['color'], 
+        (obj['xy'], 
+        (obj['r'],obj['r'])))
+
 def move(obj):
     x,y = obj['xy']
     r = obj['r']
-    xwall = x < (aboard + r) or x > (B - aboard - r)
-    ywall = y < (aboard + r) or y > (B - aboard - r)
+    xwall = x < obj['boards'][0]  or x > obj['boards'][1]
+    ywall = y < obj['boards'][2] or y > obj['boards'][3]
     if xwall:
         obj['v'][0] = (-1)*obj['v'][0]
     if ywall:
@@ -77,15 +146,20 @@ def move(obj):
 
     obj['xy'][0] += obj['v'][0]
     obj['xy'][1] += obj['v'][1]
+    #crush(obj)
 
 
 def dance():
     aboards()
     table()
     balls_full()
+    boxes_full()
     for ball in BALLS:
-        draw(ball)
+        drawBase(ball)
         move(ball)
+    for box in BOXES:
+        drawOther(box)
+        move(box)
 
 def click():
     global count
@@ -95,8 +169,15 @@ def click():
         r = ball['r']
         run = ( (x1-x)**2 + (y1-y)**2 - r**2 ) < 0
         if run:
-            count += 1
+            count += ball['count']
             BALLS.remove(ball)
+    for box in BOXES:
+        x,y = box['xy']
+        r = box['r']
+        run = ( (x1-x)**2 + (y1-y)**2 - r**2 ) < 0
+        if run:
+            count += box['count']
+            BOXES.remove(box)
 
 pygame.display.update()
 clock = pygame.time.Clock()
